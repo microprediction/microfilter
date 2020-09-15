204b565ec9acbef8fb26ffbed0c483285879a783
Peter Cotton <pcotton@intechinvestments.com>
2020-09-15 11:00:54 -0400

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="microfilter",
-    version="0.0.4",
+    version="0.0.5",
     description="Filtering",
     long_description=README,
     long_description_content_type="text/markdown",
